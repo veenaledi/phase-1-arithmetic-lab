5df40ae489aebdd94f8b632872c4d437873822e8
neema <naledineema@gmail.com>
2024-07-13 13:05:46 +0300

    index.js package-lock.json

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// Define the variables
+// Define the variables
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); 
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random)
+
+const num3 = 14
+const num4 = 5
+const mod = (num3 % num4) == 4
+console.log(mod)
+
+const numberSet = (5, 10, 15, 20, 19, 2)
+const max = math.max(numberSet)
+console.log(max)
